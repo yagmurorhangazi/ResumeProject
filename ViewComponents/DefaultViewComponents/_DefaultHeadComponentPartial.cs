f83c477e412c6b038d7729206a6ba9b815fb81a7
Yağmur Orhankazi <ezgiyagmurorhangazi@icloud.com>
2026-01-12 15:47:06 +0300

    AA

---
+++ ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProject.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
